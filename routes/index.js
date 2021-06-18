94ca59d0f6b81cffa69dee90f596f52eb3edaf17
Nathan <nathan.zhao@nine.com.au>
2021-06-18 11:33:31 +1000

    add a route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const filter = require('./filter');
+const log = require('./log')
 
 const router = express.Router();
 
+router.use('/log', log)
 router.use('/api/filter', filter);
+
 
 module.exports = router;
